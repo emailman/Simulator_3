7b0409057859b3e237e40ff834cdd6b284729415
Eric Mailman <eric.mailman@yahoo.com>
2019-12-16 15:16:53 -0500

    Simplify exercise 7b

--- exercises/exercise 7b.py
+++ exercises/exercise 7b.py
@@ -13,17 +13,11 @@ class MyUserApp(GuiApp):
         sliders = self.get_slider_values()
         temperature = sliders[0]
         self.change_message('Temperature = ' + str(temperature))
-        if 0 <= temperature < 32:
+        if temperature < 50:
             self.set_light_state('blue', 'on')
-            self.set_light_state('yellow', 'off')
-            self.set_light_state('red', 'off')
-        elif 32 <= temperature < 75:
-            self.set_light_state('blue', 'off')
-            self.set_light_state('yellow', 'on')
             self.set_light_state('red', 'off')
         else:
             self.set_light_state('blue', 'off')
-            self.set_light_state('yellow', 'off')
             self.set_light_state('red', 'on')
 
 
